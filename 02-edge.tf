c757027e8725ce90a9c3288788a6547b938299fc
damian <contact@dsieczko.co>
2022-08-03 15:49:48 +0000

    Updated to Equinix Terraform provider 1.8, fixed equinix_metal_port config in 03-esx.tf, eth1 needs to wait for eth0 to finish.

--- 02-edge.tf
+++ 02-edge.tf
@@ -1,5 +1,5 @@
 # Provision public IPs to use for the KVM public network
-resource "metal_reserved_ip_block" "routed" {
+resource "equinix_metal_reserved_ip_block" "routed" {
   project_id = var.project_id
   metro      = var.metro
   type       = "public_ipv4"
@@ -7,32 +7,32 @@ resource "metal_reserved_ip_block" "routed" {
 }
 
 # Create and configure the edge instance.
-resource "metal_device" "edge" {
+resource "equinix_metal_device" "edge" {
   hostname         = var.edge_hostname
   plan             = var.edge_size
   metro            = var.metro
   operating_system = var.edge_os
   billing_cycle    = var.billing_cycle
   project_id       = var.project_id
-  user_data        = templatefile("build-kvm-tf.sh", { pub_ip = metal_reserved_ip_block.routed.cidr_notation })
+  user_data        = templatefile("build-kvm-tf.sh", { pub_ip = equinix_metal_reserved_ip_block.routed.cidr_notation })
 }
 
 # Change network mode to hybrid-unbonded for the edge instance
-resource "metal_device_network_type" "edge" {
-  device_id  = metal_device.edge.id
+resource "equinix_metal_device_network_type" "edge" {
+  device_id  = equinix_metal_device.edge.id
   type       = "hybrid"
-  depends_on = [metal_device.edge]
+  depends_on = [equinix_metal_device.edge]
 }
 
 # Assign elastic block to the edge instance
-resource "metal_ip_attachment" "block_assignment" {
-  device_id     = metal_device.edge.id
-  cidr_notation = metal_reserved_ip_block.routed.cidr_notation
+resource "equinix_metal_ip_attachment" "block_assignment" {
+  device_id     = equinix_metal_device.edge.id
+  cidr_notation = equinix_metal_reserved_ip_block.routed.cidr_notation
 }
 
 # Add VLANs to eth1
-resource "metal_port" "edge-eth1" {
-  port_id       = [for p in metal_device.edge.ports : p.id if p.name == "eth1"][0]
-  vlan_ids      = metal_vlan.vlans.*.id
+resource "equinix_metal_port" "edge-eth1" {
+  port_id       = [for p in equinix_metal_device.edge.ports : p.id if p.name == "eth1"][0]
+  vlan_ids      = equinix_metal_vlan.vlans.*.id
   bonded        = false
 }
