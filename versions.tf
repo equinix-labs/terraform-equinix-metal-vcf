a6047b2eb4ea038e641f3cc8270f5146a524a692
vasubabu <kandimalla.vasubabu@gmail.com>
2023-05-10 14:53:20 +0530

    Add provider equinix and module_name in Equinix Metal TF configs

--- versions.tf
+++ versions.tf
@@ -2,7 +2,12 @@ terraform {
   required_providers {
     equinix = {
       source  = "equinix/equinix"
-      version = ">= 1.8.0"
+      version = "~> 1.14"
     }
+  }
+
+ required_version = ">= 0.13"
+ provider_meta "equinix" {
+    module_name = "equinix-metal-vcf"
   }
 }
