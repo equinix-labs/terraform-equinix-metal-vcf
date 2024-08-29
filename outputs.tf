c6e1641acd83adf16a8d21213b3b6777429a2a90
Chris Privitere <23177737+cprivitere@users.noreply.github.com>
2024-08-29 16:05:45 +0000

    fix: fix description to refer to VCF
    Signed-off-by: Chris Privitere <23177737+cprivitere@users.noreply.github.com>

--- outputs.tf
+++ outputs.tf
@@ -9,7 +9,7 @@ output "bastion_public_ip" {
   value       = equinix_metal_device.bastion.access_public_ipv4
 }
 output "ssh_forward_command" {
-  description = "SSH port forward command to use to connect to the Prism GUI"
+  description = "SSH port forward command to use to connect to the VCF GUIs"
   value       = "ssh -L 8000:172.16.11.101:443 -L 8100:172.16.10.3:443 -i ${module.ssh.ssh_private_key} root@${equinix_metal_device.bastion.access_public_ipv4}"
 }
 output "management_public_ip" {
