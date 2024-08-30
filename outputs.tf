53b0f58c19741b1a5a2887345ea23ea17d552123
Chris Privitere <23177737+cprivitere@users.noreply.github.com>
2024-08-30 16:19:52 +0000

    feat: Update outputs to give directions to connect to windows host.
    Signed-off-by: Chris Privitere <23177737+cprivitere@users.noreply.github.com>

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,24 @@
 
 output "ssh_private_key" {
-  description = "The private key for the SSH keypair"
+  description = "SSH Private key to use to connect to bastion and management hosts over SSH."
   value       = module.ssh.ssh_private_key
   sensitive   = false
 }
 output "bastion_public_ip" {
-  description = "The public IP address of the bastion host"
+  description = "The public IP address of the bastion host."
   value       = equinix_metal_device.bastion.access_public_ipv4
 }
-output "ssh_forward_command" {
-  description = "SSH port forward command to use to connect to the VCF GUIs"
-  value       = "ssh -L 8000:172.16.11.101:443 -L 8100:172.16.10.3:443 -i ${module.ssh.ssh_private_key} root@${equinix_metal_device.bastion.access_public_ipv4}"
-}
 output "management_public_ip" {
-  description = "The public IP address of the windows management host"
+  description = "The public IP address of the windows management host."
   value       = equinix_metal_device.management.access_public_ipv4
 }
-
 output "management_password" {
   description = "Randomly generated password used for the Admin accounts on the management host."
   sensitive   = true
-  value       = random_password.management.result
+  value       = "Management Host Admin password: ${random_password.management.result}\n"
+}
+
+output "next_steps" {
+  description = "Instructions for accessing the management host."
+  value       = "Run `terraform output -raw management_password` to see the management host's admin password and then RDP to the Management host as SYSTEM\\Admin with that password."
 }
